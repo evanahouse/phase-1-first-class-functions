8bc58076fec51c4bb5a9e4eb432377c077225d0d
Evan <evanahouse@gmail.com>
2021-02-23 21:57:00 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback){
+    callback();
+}
+
+function returnsANamedFunction(){
+    return namedFunction;
+}
+
+const namedFunction = () => 'Named Function'
+
+function returnsAnAnonymousFunction() {
+    return function (){
+        console.log('Hi');
+    }
+}
+
